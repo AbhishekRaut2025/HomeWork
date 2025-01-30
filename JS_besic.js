5ffcc64931948c1b514011f13179c44752f36775
AbhishekRaut8 <abhishekraut4412@gmail.com>
2025-01-30 16:01:12 +0530

    Added files

---
+++ JS_besic.js
@@ -0,0 +1,18 @@
+
+function apple(){
+    console.log("apple")
+}
+
+console.log("hello")
+
+ var a=1
+    while (a<6){
+    console.log(a)
+    a++
+    }
+
+    for(var i=1; i<6; i++){
+    console.log(i)
+    }
+
+apple();
